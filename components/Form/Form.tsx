0e5ceb28f307bed66b31e145c033de1626784fa6
dmitryshur <dimashur@gmail.com>
2021-03-10 19:21:29 +0300

    Initial commit

---
+++ components/Form/Form.tsx
@@ -0,0 +1,7 @@
+import styles from './Form.module.css';
+
+function Form() {
+  return <div>This is a form</div>;
+}
+
+export default Form;
---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+import Form from 'components/Form/Form';
+import styles from './index.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.Home}>
+      <Form />
+    </div>
+  );
+}
